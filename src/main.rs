1f6e2c9dcfb2899cdd28b05aef7f8572598ee85f
Takanori Ishibashi <takanori.1112@gmail.com>
2016-11-25 07:59:07 +0900

    Add option

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![feature(rustc_private)]
+
+extern crate getopts;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().map(|x| x.to_string())
+                                       .collect();
+    let ref program = args[0];
+
+    let opts = [
+        getopts::optflag("n", "", "do not output the trailing newline"),
+        getopts::optflag("h", "help", "display this help and exit"),
+        getopts::optflag("V", "version",
+                         "output version information and exet"),
+    ];
 }
